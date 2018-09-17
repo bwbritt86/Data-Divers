e43fcdf9fbcd9c96466ce86e8c829d65ccc7fdfd
Cody Buchanan <hitherewhadup@outlook.com>
2018-09-16 19:31:32 -0500

    Setting up infrsatucewsv

--- HoustonDataVisualizer/HoustonDataVisualizer/Properties/AssemblyInfo.cs
+++ HoustonDataVisualizer/HoustonDataVisualizer/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4c6f128b-1365-453b-afce-cc564e9747ae")]
+[assembly: Guid("d23ca61f-7119-4cff-b7e4-d54278f48c17")]
 
 // Version information for an assembly consists of the following four values:
 //
