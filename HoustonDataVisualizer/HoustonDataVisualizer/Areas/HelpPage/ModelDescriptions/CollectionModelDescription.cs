ac4dce1512991aec80515c9f660283d9da8ffc3b
Cody Buchanan <hitherewhadup@outlook.com>
2018-09-16 19:03:31 -0500

    yes

--- HoustonDataVisualizer/HoustonDataVisualizer/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace HoustonDataVisualizer.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- HoustonDataVisualizer/HoustonDataVisualizer/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HoustonDataVisualizer.MvcApplication" Language="C#" %>
