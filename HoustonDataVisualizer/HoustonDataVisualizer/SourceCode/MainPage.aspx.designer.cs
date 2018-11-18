0e07bc6845ae3f129fd66216e79896096891ab3b
Cody Buchanan <hitherewhadup@outlook.com>
2018-11-18 17:29:01 -0600

    Updated textbox for asp

--- HoustonDataVisualizer/HoustonDataVisualizer/SourceCode/MainPage.aspx.designer.cs
+++ HoustonDataVisualizer/HoustonDataVisualizer/SourceCode/MainPage.aspx.designer.cs
@@ -31,6 +31,15 @@ namespace HoustonDataVisualizer.SourceCode {
         protected global::System.Web.UI.ScriptManager ScriptManager1;
         
         /// <summary>
+        /// addressMap control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox addressMap;
+        
+        /// <summary>
         /// DropDownList1 control.
         /// </summary>
         /// <remarks>
